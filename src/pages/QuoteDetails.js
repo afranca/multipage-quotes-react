c65a46f3b4d4f8c8c3c08afd59d9c934122c1999
Alexandre Franca <Alexandre.Franca@bjss.com>
2023-01-02 09:27:27 +0000

    feat: adding View Comments functionality

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Route, useParams } from "react-router-dom";
-import Comments from '../components/comments/Comments';
-import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import { Route, useParams, useHistory, Link } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const DUMMY_QUOTES = [
   {
@@ -28,16 +28,24 @@ const DUMMY_QUOTES = [
 
 const QuoteDetails = () => {
   const params = useParams();
-  
-  const quote  = DUMMY_QUOTES.find((qte) => qte.id === params.quoteId );
-  
-  if (!quote){
-    return <p>No quote found</p>
+  const history = useHistory();
+  const quote = DUMMY_QUOTES.find((qte) => qte.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found</p>;
   }
 
   return (
     <React.Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author}/>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            View Comments
+          </Link>
+        </div>
+      </Route>
+
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
