d9e1009a9cb9abf9b40c5b44b6d959d25c77d350
Alexandre Franca <Alexandre.Franca@bjss.com>
2023-01-02 08:05:24 +0000

    feat: implementing QuoteDetails page (using dummy data for now)

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,13 +1,43 @@
 import React from "react";
 import { Route, useParams } from "react-router-dom";
-import Comments from '../components/comments/Comments'
+import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Albert Einstein",
+    text: "Two things are infinite: the universe and human stupidity; and I am not sure about the universe",
+  },
+  {
+    id: "q2",
+    author: "Mahatma Gandhi",
+    text: "Be the change that you wish to see in the world",
+  },
+  {
+    id: "q3",
+    author: "Frank Zappa",
+    text: "Without deviation from the norm, progress is not possible",
+  },
+  {
+    id: "q4",
+    author: "Richard Dawkins",
+    text: "We are all atheists about most of the gods that humanity has ever believed in. Some of us just go one god further",
+  },
+];
 
 const QuoteDetails = () => {
   const params = useParams();
+  
+  const quote  = DUMMY_QUOTES.find((qte) => qte.id === params.quoteId );
+  
+  if (!quote){
+    return <p>No quote found</p>
+  }
+
   return (
     <React.Fragment>
-      <h1>Quote Details </h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author}/>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
