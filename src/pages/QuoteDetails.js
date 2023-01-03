4801324afa12b1eff251e7fc8dbe711f509333db
Alexandre Franca <Alexandre.Franca@bjss.com>
2023-01-03 11:31:06 +0000

    feat: Http Request for Quote Details

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,44 +1,41 @@
-import React from "react";
-import { Route, useParams, useHistory, Link, useRouteMatch } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, useParams, Link, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
-const DUMMY_QUOTES = [
-  {
-    id: "q1",
-    author: "Albert Einstein",
-    text: "Two things are infinite: the universe and human stupidity; and I am not sure about the universe",
-  },
-  {
-    id: "q2",
-    author: "Mahatma Gandhi",
-    text: "Be the change that you wish to see in the world",
-  },
-  {
-    id: "q3",
-    author: "Frank Zappa",
-    text: "Without deviation from the norm, progress is not possible",
-  },
-  {
-    id: "q4",
-    author: "Richard Dawkins",
-    text: "We are all atheists about most of the gods that humanity has ever believed in. Some of us just go one god further",
-  },
-];
-
-const QuoteDetails = () => {
+const QuoteDetails = () => {  
   const match = useRouteMatch();
   const params = useParams();
-  //const history = useHistory();
-  const quote = DUMMY_QUOTES.find((qte) => qte.id === params.quoteId);
+  
+  const { quoteId } = params; 
+  const { sendRequest, status, data: loadedQuote, error } = useHttp(getSingleQuote, true);
+  
+  useEffect(() => {
+    sendRequest(quoteId);  
+  }, [sendRequest,quoteId]);
 
-  if (!quote) {
-    return <p>No quote found</p>;
+  if (status === 'pending'){
+    return <div className='centered'>
+      <LoadingSpinner />
+    </div>
   }
-  console.log(match);
+
+  if (error){
+    return <p className='centered focused'>
+      {error}
+    </p>
+  }
+  
+  if (!loadedQuote.text) {
+     return <p>No quote found</p>;
+   }  
+  
   return (
     <React.Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={match.path} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${match.url}/comments`}>
