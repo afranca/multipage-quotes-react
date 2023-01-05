087a8c2072f2ef56a163fd9a069aaa66fb7a1680
Alexandre Franca <Alexandre.Franca@bjss.com>
2023-01-05 09:12:25 +0000

    feat: Adding and Listing comments of a specific quote

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,10 +1,39 @@
-import { useState } from 'react';
-
+import { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+import useHttp from '../../hooks/use-http';
+import {getAllComments } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import CommentsList from './CommentsList';
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const {sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  const params = useParams();
+  const { quoteId } = params;
+
+  useEffect(() =>{
+    sendRequest(quoteId);
+  },[quoteId, sendRequest]);
+
+  let comments;
+  const commentAddedHandler = useCallback(() =>{ 
+    sendRequest(quoteId);
+  },[sendRequest, quoteId]);
+
+  if (status === 'pending'){
+    comments = <div className='centered'> <LoadingSpinner /> </div>
+  }
+  
+  if (status === 'completed' && (loadedComments && loadedComments.length > 0)){
+    comments = <CommentsList comments={loadedComments}/>
+  }
+
+  if (status === 'completed' && (!loadedComments || loadedComments.length === 0)){
+    comments = <p className='centered'>No Comments yet!</p>
+  }
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
@@ -18,8 +47,10 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && <NewCommentForm quoteId={quoteId} onCommentAdded={commentAddedHandler}/>}
+      
+      { comments}
+
     </section>
   );
 };
